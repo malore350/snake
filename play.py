f6d36938a66c5c62c1787409d931f85341773f92
Gasimov Kamran <kamran.qasimoff@kaist.ac.kr>
2024-06-04 03:43:07 +0900

    fixed bugs

--- play.py
+++ play.py
@@ -5,36 +5,36 @@ from dqn import DQNAgent
 import pygame
 
 def play(render=True):
-    # Run the start screen function
-    animations = GameScreen()
-    animations.start_screen()
+      # Run the start screen function
+      animations = GameScreen()
+      animations.start_screen()
 
-    game = SnakeGameAI()
-    state_dim = game.get_state().shape[0]
-    action_dim = 4  # Number of possible actions: up, down, left, right
-    agent = DQNAgent(state_dim, action_dim)
+      game = SnakeGameAI()
+      state_dim = game.get_state().shape[0]
+      action_dim = 4  # Number of possible actions: up, down, left, right
+      agent = DQNAgent(state_dim, action_dim)
 
-    # Load the model's state dictionary
-    agent.model.load_state_dict(torch.load('snake_dqn_weights_2.pth'))
-    agent.model.eval()  # Set the model to evaluation mode
-    print("Model weights loaded.")
+      # Load the model's state dictionary
+      agent.model.load_state_dict(torch.load('snake_dqn_weights_2.pth'))
+      agent.model.eval()  # Set the model to evaluation mode
+      print("Model weights loaded.")
 
-    agent.epsilon = 0
+      agent.epsilon = 0
 
-    state = game.reset()
-    done = False
-    step = 0
+      state = game.reset()
+      done = False
+      step = 0
 
-    while not done:
-        action = agent.act(state)
-        state, reward, done = game.step(action)
+      while not done:
+            action = agent.act(state)
+            state, reward, done = game.step(action)
 
-        if render:
+            if render:
             game.render()
-        step += 1
-    animations.game_over_screen()
-    print(f"Game over! Total steps: {step}, Score: {game.score}")
+            step += 1
+      animations.game_over_screen()
+      print(f"Game over! Total steps: {step}, Score: {game.score}")
 
 if __name__ == "__main__":
-    while True:
-        play(render=True)
+      while True:
+            play(render=True)
